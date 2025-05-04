9c4ddb926daa3f15ae0977c51f6d0927eabbfa5a
mon-doo <monica.dooley722@gmail.com>
2025-05-04 04:01:34 -0500

    com

--- creative-dat-vis.R
+++ creative-dat-vis.R
@@ -1,1 +1,11 @@
 #Script
+
+library(xml2)
+library(XML)
+dat <- read_xml("https://raw.githubusercontent.com/mon-doo/creative-data-visualization/refs/heads/main/DOC0002.XML")
+dat <- xmlParse(dat, useInternalNodes=TRUE, 
+                addAttributeNamespaces=TRUE)
+procedure_list <- getNodeSet(dat, "//ClinicalDocument/component/structuredBody/component/section/entry/organizer/component/act")
+dat2 <- xmlToDataFrame(dat, nodes = procedure_list, homogenous = FALSE)
+dat2 <- xmlToDataFrame(dat2)
+
